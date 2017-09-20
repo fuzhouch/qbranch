415189849b34d68bd8c0e3827aabe95c29de5696
fuzhou.chen <fuzhouch@gmail.com>
2017-09-20 09:19:59 +0800

    [New] Set correct interface for all builtin type args.

--- core/src/main/kotlin/net/dummydigit/qbranch/generic/BuiltinTypeArg.kt
+++ core/src/main/kotlin/net/dummydigit/qbranch/generic/BuiltinTypeArg.kt
@@ -5,18 +5,18 @@ package net.dummydigit.qbranch.generic
 import net.dummydigit.qbranch.types.*
 
 object BuiltinTypeArg {
-    @JvmField val Int8T = StructT<Byte>({ 0 })
-    @JvmField val Int16T = StructT<Short>({ 0 })
-    @JvmField val Int32T = StructT({ 0 })
-    @JvmField val Int64T = StructT<Long>({ 0 })
-    @JvmField val UInt8T = StructT(UnsignedByte::class)
-    @JvmField val UInt16T = StructT(UnsignedShort::class)
-    @JvmField val UInt32T = StructT(UnsignedInt::class)
-    @JvmField val UInt64T = StructT(UnsignedLong::class)
-    @JvmField val ByteStringT = StructT(ByteString::class)
-    @JvmField val WStringT = StructT({ "" })
-    @JvmField val FloatT = StructT({ 0.0f })
-    @JvmField val DoubleT = StructT({ 0.0 })
-    @JvmField val BlobT = StructT(Blob::class)
-// TODO: Bonded?
+    @JvmField val Int8T : TypeArg<Byte> = StructT<Byte>({ 0 })
+    @JvmField val Int16T : TypeArg<Short> = StructT<Short>({ 0 })
+    @JvmField val Int32T : TypeArg<Int> = StructT({ 0 })
+    @JvmField val Int64T : TypeArg<Long> = StructT<Long>({ 0 })
+    @JvmField val UInt8T : TypeArg<UnsignedByte> = StructT(UnsignedByte::class)
+    @JvmField val UInt16T : TypeArg<UnsignedShort> = StructT(UnsignedShort::class)
+    @JvmField val UInt32T : TypeArg<UnsignedInt> = StructT(UnsignedInt::class)
+    @JvmField val UInt64T : TypeArg<UnsignedLong> = StructT(UnsignedLong::class)
+    @JvmField val ByteStringT : TypeArg<ByteString> = StructT(ByteString::class)
+    @JvmField val WStringT : TypeArg<String> = StructT({ "" })
+    @JvmField val FloatT : TypeArg<Float> = StructT({ 0.0f })
+    @JvmField val DoubleT : TypeArg<Double> = StructT({ 0.0 })
+    @JvmField val BlobT : TypeArg<Blob> = StructT(Blob::class)
+    // TODO: Bonded?
 }
